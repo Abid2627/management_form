1455d692d1b78e27f4f9a8fe22347ac395714670
Abid2627 <shaikabid2023@gmail.com>
2024-03-06 17:02:53 +0530

    updated files

--- join_management/forms.py
+++ join_management/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import ManagementJoiningForm
+from .models import JoinManagement
 
 class JoinManagementForm(forms.ModelForm):
     BLOOD_GROUP_CHOICES = [
@@ -16,9 +16,20 @@ class JoinManagementForm(forms.ModelForm):
     blood_group = forms.ChoiceField(choices=BLOOD_GROUP_CHOICES)
 
     class Meta:
-        model = ManagementJoiningForm
+        model = JoinManagement
         fields = '__all__'
         widgets = {
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
             'joining_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        phone_number = cleaned_data.get('phone_number')
+        if JoinManagement.objects.filter(email=email).exists():
+            self.add_error('email', 'This email address is already registered.')
+        if JoinManagement.objects.filter(phone_number=phone_number).exists():
+            self.add_error('phone_number', 'This phone number is already registered.')
+
+        return cleaned_data
